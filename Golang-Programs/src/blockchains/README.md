19156de3c317831f0db1ec4c6153b292ccd3440d
Ravi Prakash <1907prakash.ravi@gmail.com>
2021-07-16 08:03:44 +0530

    basics

---
+++ Golang-Programs/src/blockchains/README.md
@@ -0,0 +1,7 @@
+# Highlights  
+
+* The program uses the _BadgerDB_ for persistent storage.  
+* _BadgerDB_ is a key-value data store unit.  
+* Bitcoin and many other cryptocurrencies use _LevelDB_.  
+* _BadgerDB_ is based upon the _LevelDB_, itself.  
+* _BadgerDB_ is a native Golang.  
--- Golang-Programs/src/blockchains/blockchain/block.go
+++ Golang-Programs/src/blockchains/blockchain/block.go
@@ -1,9 +1,9 @@
 package blockchain
 
-// a chain of blocks
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+)
 
 // each block gonna have a data along with a hash
 // this block will attatch to last (previous) block
@@ -26,16 +26,38 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
-}
-
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+func  (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	Handle(err)
+
+	return res.Bytes()
 }
+
+
+func  (data []byte) Deerialize() *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(b)
+
+	Handle(err)
+
+	return &block
+}
+
+
+// handle the errors
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
