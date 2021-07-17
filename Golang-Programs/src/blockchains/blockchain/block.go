15dad7fca1e8aeb716d95fef86d8d90cfda33146
Ravi Prakash <1907prakash.ravi@gmail.com>
2021-07-17 12:41:18 +0530

    basics of BCT

--- Golang-Programs/src/blockchains/blockchain/block.go
+++ Golang-Programs/src/blockchains/blockchain/block.go
@@ -1,10 +1,9 @@
 package blockchain
 
-import (
-	"log"
-	"bytes"
-	"encoding/gob"
-)
+// a chain of blocks
+type BlockChain struct {
+	Blocks []*Block
+}
 
 // each block gonna have a data along with a hash
 // this block will attatch to last (previous) block
@@ -27,38 +26,16 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, new)
+}
+
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func  (b *Block) Serialize() []byte {
-	var res bytes.Buffer
-	encoder := gob.NewEncoder(&res)
-
-	err := encoder.Encode(b)
-
-	Handle(err)
-
-	return res.Bytes()
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
 }
-
-
-func  Deserialize(data []byte) *Block {
-	var block Block
-	decoder := gob.NewDecoder(bytes.NewReader(data))
-
-	err := decoder.Decode(&block)
-
-	Handle(err)
-
-	return &block
-}
-
-
-// handle the errors
-func Handle(err error) {
-	if err != nil {
-		log.Panic(err)
-	}
-}
-
